774ae9be2ca4de50cf1c4aed3a181eb6815d18e4
Furentsu <eugeniofranzoso@gmail.com>
2021-09-14 17:16:49 +0200

    Added a random image

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const root = new Vue({
     el: '#root',
     data: {
-      title: "My first Vue application!!!"
+      title: "My first Vue application!!!",
+      image: `https://source.unsplash.com/random/1920x1080?sig=${Math.random()}`,
+      height: 600
     }
   })
